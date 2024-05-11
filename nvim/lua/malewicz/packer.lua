d6c6cfd20b771a544545d015f818ff41e9526348
Daniil Malewicz <daniil.adamov.2001@gmail.com>
2024-05-11 10:59:45 -0500

    Added copilot remaps

--- nvim/lua/malewicz/packer.lua
+++ nvim/lua/malewicz/packer.lua
@@ -1,94 +1,95 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    use({
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-    use "nvim-lua/plenary.nvim"
-    use {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        requires = { { "nvim-lua/plenary.nvim" } }
-    }
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
+	use("nvim-lua/plenary.nvim")
+	use({
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            --- Uncomment the two plugins below if you want to manage the language servers from neovim
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-            { 'neovim/nvim-lspconfig' },
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-        }
-    }
+			{ "neovim/nvim-lspconfig" },
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	})
 
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional
-        },
-    }
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	})
 
-    use { 'christoomey/vim-tmux-navigator' }
+	use({ "christoomey/vim-tmux-navigator" })
 
-    use {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = function()
-            require("nvim-autopairs").setup({
-                check_ts = true,
-                ts_config = {
-                    lua = { 'string' }, -- it will not add a pair on that treesitter node
-                    javascript = { 'template_string' },
-                    java = false,       -- don't check treesitter on java
-                }
-            })
-        end
-    }
+	use({
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({
+				check_ts = true,
+				ts_config = {
+					lua = { "string" }, -- it will not add a pair on that treesitter node
+					javascript = { "template_string" },
+					java = false, -- don't check treesitter on java
+				},
+			})
+		end,
+	})
 
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
 
-    use({
-        "stevearc/conform.nvim",
-        config = function()
-            require("conform").setup()
-        end,
-    })
+	use({
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup()
+		end,
+	})
 
-    use("mfussenegger/nvim-lint")
+	use("mfussenegger/nvim-lint")
 
-    use ("github/copilot.vim")
+	use("github/copilot.vim")
 end)
