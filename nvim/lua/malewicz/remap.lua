5c6d4584851b4e381df2192069d12a5aca76feca
Malewicz <132208688+malewicz1337@users.noreply.github.com>
2024-07-07 15:48:07 +0200

    Update remap.lua

--- nvim/lua/malewicz/remap.lua
+++ nvim/lua/malewicz/remap.lua
@@ -51,3 +51,8 @@ vim.keymap.set("n", "<leader><leader>", function()
 end)
 
 vim.api.nvim_set_keymap("i", "<CR>", "<CR>", { noremap = true })
+
+-- vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+-- vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+-- vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+-- vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
