5e700b3df32e55dfec054a0d0fd5cc5d3db8cc0a
Daniil Malewicz <daniil.adamov.2001@gmail.com>
2025-09-19 18:10:09 +0400

    wezterm updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,37 +6,36 @@ if wezterm.config_builder then
 end
 
 config = {
-	default_cursor_style = "BlinkingBlock",
+	default_cursor_style = "SteadyBlock",
 	force_reverse_video_cursor = false,
 	automatically_reload_config = true,
 	window_close_confirmation = "NeverPrompt",
 	adjust_window_size_when_changing_font_size = false,
 	window_decorations = "RESIZE",
-	check_for_updates = false,
+	check_for_updates = true,
 	use_fancy_tab_bar = false,
 	tab_bar_at_bottom = false,
 	enable_tab_bar = true,
-	font_size = 16,
+	font_size = 24,
 	font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Bold" }),
 	window_padding = {
-		left = 3,
-		right = 3,
+		left = 0,
+		right = 0,
 		top = 0,
 		bottom = 0,
 	},
 	background = {
 		{
 			source = {
-				File = "/Users/" .. os.getenv("USER") .. "/.config/wezterm/neovim.jpg",
+				File = "/Users/" .. os.getenv("USER") .. "/.config/wezterm/frieren.jpg",
 			},
 			hsb = {
 				hue = 1.0,
 				saturation = 1.02,
-				brightness = 0.25,
+				brightness = 1.00,
 			},
-			-- attachment = { Parallax = 0.3 },
-			-- width = "100%",
-			-- height = "100%",
+			width = "100%",
+			height = "100%",
 		},
 		{
 			source = {
@@ -44,7 +43,7 @@ config = {
 			},
 			width = "100%",
 			height = "100%",
-			opacity = 0.55,
+			opacity = 0.8,
 		},
 	},
 	hyperlink_rules = {
@@ -81,6 +80,11 @@ config = {
 
 	keys = {
 		{
+			key = ";",
+			mods = "CTRL",
+			action = wezterm.action.SendKey({ key = "F13" }),
+		},
+		{
 			key = "=",
 			mods = "CMD",
 			action = wezterm.action.IncreaseFontSize,
@@ -90,14 +94,41 @@ config = {
 			mods = "CMD",
 			action = wezterm.action.DecreaseFontSize,
 		},
+		{
+			key = "p",
+			mods = "CTRL",
+			action = wezterm.action.SendKey({ key = "b", mods = "ALT" }),
+		},
+		{
+			key = "n",
+			mods = "CTRL",
+			action = wezterm.action.SendKey({ key = "f", mods = "ALT" }),
+		},
+		{
+			key = "a",
+			mods = "CTRL",
+			action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+		},
+		{
+			key = "e",
+			mods = "CTRL",
+			action = wezterm.action.SendKey({ key = "e", mods = "CTRL" }),
+		},
+		{
+			key = "w",
+			mods = "CTRL",
+			action = wezterm.action.SendKey({ key = "w", mods = "CTRL" }),
+		},
+		{
+			key = "Backspace",
+			mods = "ALT",
+			action = wezterm.action.SendKey({ key = "Backspace", mods = "ALT" }),
+		},
 	},
 	colors = {
 		cursor_bg = "#ffffff",
-		cursor_fg = "#000000",
+		cursor_fg = "#282c35",
 		cursor_border = "#ffffff",
 	},
-	cursor_blink_ease_in = "Constant",
-	cursor_blink_ease_out = "Constant",
-	cursor_blink_rate = 600,
 }
 return config
